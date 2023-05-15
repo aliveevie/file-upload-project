1032e75a4a9671c84a12972f71f991ca7656bf66
Ibrahim Abdulkarim <aliveibi080@gmail.com>
2023-05-15 05:07:12 +0000

    The final project

--- index.js
+++ index.js
@@ -1,15 +1,30 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+const multer = require('multer')
 
 var app = express();
+
+const upload = multer({dest: 'uploads/'})
+
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
+
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
+
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  const response = {
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+
+  }  
+  res.json(response)
+})
 
 
 
